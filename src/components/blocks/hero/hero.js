53dee46b2735619d145589a402caa230cce1147e
zenzubel <deyevnv@gmail.com>
2025-02-20 12:52:49 +0700

    Завершен слайдер на главной + адаптив

--- src/components/blocks/hero/hero.js
+++ src/components/blocks/hero/hero.js
@@ -1,34 +1,48 @@
 function initSliderDefault() {
-  const $sliders = document.querySelectorAll("[data-slider-fade");
-  if ($sliders.length) {
-    $sliders.forEach(($slider) => {
-      const $pagination = $slider.querySelector(".swiper-pagination") || null;
+	const $sliders = document.querySelectorAll("[data-slider-fade");
+	const $mediaSm = window.matchMedia('(max-width: 768px)');
+	if ($sliders.length) {
+		$sliders.forEach(($slider) => {
+			const $pagination = $slider.querySelector(".swiper-pagination");
+			const $paginationText = $slider.querySelectorAll("[data-pagination-text]");
+			let  pagArray = []
 
-        const slider1 = new Swiper($slider, {
-          slidesPerView: "auto",
-          spaceBetween: 20,
-          speed: 400,
-          centeredSlides: false,
-          watchOverflow: true,
-          resizeObserver: true,
-          allowTouchMove: true,
-          slideVisibleClass: "active",
-          pagination: {
-            el: $pagination,
-          },
-          effect: "fade",
-          fadeEffect: {
-            crossFade: true,
-          },
-          breakpoints: {
-            768: {
-              spaceBetween: 20,
-            },
-          },
-        });
-    });
-  }
+			$paginationText.forEach(item => {
+				let text = item.getAttribute('data-pagination-text');
+				pagArray.push(text);
+			});
+
+			const slider1 = new Swiper($slider, {
+				slidesPerView: "auto",
+				speed: 1200,
+				centeredSlides: false,
+				watchOverflow: true,
+				resizeObserver: true,
+				allowTouchMove: true,
+				slideVisibleClass: "active",
+				pagination: {
+					el: $pagination,
+					clickable: true,
+					renderBullet: function (index, bullet) {
+						return '<span class="' + bullet + '">' + (pagArray[index]) + '</span>';
+					},
+				},
+				effect: "fade",
+				fadeEffect: {
+					crossFade: true,
+				},
+			});
+
+			if(!$mediaSm.matches) {
+				const $paginationBullets = $pagination.querySelectorAll(".swiper-pagination-bullet");
+				$paginationBullets.forEach((bullet, i) => {
+					bullet.addEventListener('mouseenter', ()=> {
+						slider1.slideTo(i);
+					})
+				});
+			}
+		});
+	}
 }
-
 window.addEventListener("load", initSliderDefault);
 
